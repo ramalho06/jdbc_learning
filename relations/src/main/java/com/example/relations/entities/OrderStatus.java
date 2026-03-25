88e0bc4d883b3c0c5850de6cf3c28954e26ff176
ramalho06 <150079153+ramalho06@users.noreply.github.com>
2026-03-25 19:48:12 -0300

    Delete relations directory

--- relations/src/main/java/com/example/relations/entities/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.relations.entities;
-
-public enum OrderStatus {
-    WAITING_PAYMENT,
-    PAID,
-    SHIPPED,
-    DELIVERED,
-    CANCELED;
-}
--- relations/src/main/resources/db/migration/V1__Create_User_Table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_user
-(
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255)        NOT NULL,
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    phone      VARCHAR(20),
-    birth_date DATE,
-    password   VARCHAR(255)        NOT NULL
-);
